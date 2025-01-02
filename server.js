a5c6b681ca86f5dd464570a990866bef36e81b10
Ibtissemkr <ibtissem.kraiem2@gmail.com>
2025-01-02 23:36:13 +0100

    Configure MongoDB connection

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require ('express');
 const app = express();
+const connectDB = require('./config/db');
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+connectDB();
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
